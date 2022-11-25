283a7b12b53f53670e2c53fc84aaae9cfc3b4fd0
HenriqueFRodrigues <fernandeshenriquer92@gmail.com>
2022-11-25 20:07:38 -0300

    adição de alguns comandos de adição de arrays, tanto no começo como no final da frase, comandos de deletar também, e do .indexOf para encontrar o número ao quala  string se refere.

--- 1-Fundamentos/secao-04-introducao-javascript/dia-02/exercicio-aula-1.js
+++ 1-Fundamentos/secao-04-introducao-javascript/dia-02/exercicio-aula-1.js
@@ -1,19 +1,24 @@
 let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
 
-tasksList.push("Cuidar do bebê");
-tasksList.unshift("Dormir");
+//tasksList.push("Cuidar do bebê"); //acrescenta mais um valor para o array no final dos valores
+//tasksList.unshift("Dormir"); // acrescenta um valor ao array no index [0]
+//tasksList.pop(); // retira o ultimo da lista
+//tasksList.shift(); // retira o primeiro da lista
+
+let indexOfTask = tasksList.indexOf('Reunião');
+console.log(indexOfTask); //  Demonstra o numero da array de tal frase, no caso 1
 
 for (let index = 0; index < tasksList.length; index +=1){
     console.log(tasksList[index]);
 }
 
-let searchForFirstTask = tasksList[0];
-console.log("Procurando o primeiro item da lista: " + searchForFirstTask);
+//let searchForFirstTask = tasksList[0];
+//console.log("Procurando o primeiro item da lista: " + searchForFirstTask);
 
 
-let searchForLastTask = tasksList[tasksList.length - 1];
-console.log("Procurando o ultimo item da lista: " + searchForLastTask);
-console.log(tasksList.length);
+//let searchForLastTask = tasksList[tasksList.length - 1];
+//console.log("Procurando o ultimo item da lista: " + searchForLastTask);
+//console.log(tasksList.length);
 
 //let foundMidleTasks = tasksList[tasksList.length / 2];
 //console.log(foundMidleTasks); >>>>>>> tentar achar por outro for <<<<<<<<<<<<
