c2afc92a3a3f3c9c5c9bef3f8841f29a27c5f683
HenriqueFRodrigues <fernandeshenriquer92@gmail.com>
2022-11-25 20:13:57 -0300

    criação do arquivo <fixando-os-exercicios-da-aula-1.js> fazendo os exercicios e deixando comentarios

---
+++ 1-Fundamentos/secao-04-introducao-javascript/dia-02/fixando-os-exericios-da-aula-1.js
@@ -0,0 +1,15 @@
+// 1-Obtenha o valor “Serviços” do array menu
+
+
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices = menu.indexOf('Serviços');
+console.log(menuServices);
+
+//2-Procure o índice do valor “Portfólio” do array menu: 
+
+let indexOfPortfolio = menu.indexOf('Portfólio');
+console.log(indexOfPortfolio);
+
+//3-Adicione o valor “Contato” no final do array menu:
+menu.push('Contato');
+console.log(menu);
