21573c911c532e43385ffc0e4a7cffa9296c2d8a
HenriqueFRodrigues <fernandeshenriquer92@gmail.com>
2022-11-25 16:08:00 -0300

    fazendo um for, imprindo toda a string composta de tasksList e adicionando mais um valor na string

---
+++ 1-Fundamentos/secao-04-introducao-javascript/dia-02/exercicio-aula.js
@@ -0,0 +1,6 @@
+let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+
+tasksList.push("Cuidar do bebê");
+for (let index = 0; index < tasksList.length; index +=1){
+    console.log(tasksList[index]);
+}
