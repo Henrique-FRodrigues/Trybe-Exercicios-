611079c695226ae68896de6bef6198a6aa469c9a
HenriqueFRodrigues <fernandeshenriquer92@gmail.com>
2022-11-26 01:10:06 -0300

    adicionando exercicio do tabuleiro

--- 1-Fundamentos/secao-04-introducao-javascript/dia-01/exercicios-agora-e-pratica.js
+++ 1-Fundamentos/secao-04-introducao-javascript/dia-01/exercicios-agora-e-pratica.js
@@ -71,3 +71,20 @@ else if (number < 0){
 else {
     console.log("Zero");
 }
+
+//Utilize if/else para escrever um código que defina três constantes com os valores dos três 
+//ângulos internos de um triângulo. Retorne true se os ângulos representarem os ângulos de um 
+//triângulo e false, caso contrário. Se algum ângulo for inválido, você deve retornar uma 
+//mensagem de erro.
+//Para os ângulos serem de um triângulo válido, a soma dos três deve ser 180 graus.
+//Um ângulo será considerado inválido se não tiver um valor positivo.
+const A = 45;
+const B = 75;
+const C = 60;
+
+if (A + B + C === 180  && A > 0 && B > 0 && C > 0) {
+    console.log(true);
+}
+else {
+    console.log(false);
+}
