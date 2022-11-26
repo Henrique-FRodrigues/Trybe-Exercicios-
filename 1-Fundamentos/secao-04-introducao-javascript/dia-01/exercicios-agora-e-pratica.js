2cf76eb82805c228d2af0ad359594d183d18edcc
HenriqueFRodrigues <fernandeshenriquer92@gmail.com>
2022-11-25 22:24:08 -0300

    adição de operações aritméticas

--- 1-Fundamentos/secao-04-introducao-javascript/dia-01/exercicios-agora-e-pratica.js
+++ 1-Fundamentos/secao-04-introducao-javascript/dia-01/exercicios-agora-e-pratica.js
@@ -1,4 +1,16 @@
+//Exercícios - agora, a prática
+//
+//Nos exercícios de hoje, você vai elaborar alguns códigos de acordo com o que é pedido no enunciado. Todos trabalham a lógica condicional (if/else e switch/case), operadores aritméticos (+, -, *, /, %) e operadores lógicos (>, <, &&, ||). Para que consiga executar seus códigos, recomendamos que utilize a extensão Code Runner. Você pode ver mais sobre ela no conteúdo que fizemos sobre o uso do VS Code.
+//
+//    Elabore alguns códigos e imprima o resultado no console usando o console.log, um para cada operação aritmética básica. Seu código deve ter duas constantes, a e b, definidas no começo com os valores que serão operados. Escreva códigos para:
+//        Adição (a + b)
+//       Subtração (a - b)
+//        Multiplicação (a * b)
+//        Divisão (a / b)
+//        Módulo (a % b)
+
+
 const a = 5;
 const b = 10;
-let sub = a+b;
+let sub = a + b;
 console.log(sub);
