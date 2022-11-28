5ab8e0ab87cc95671612cf888f451a7a413b6c8e
HenriqueFRodrigues <fernandeshenriquer92@gmail.com>
2022-11-28 10:05:42 -0300

    exercicio das notas

--- 1-Fundamentos/secao-04-introducao-javascript/dia-01/exercicios-agora-e-pratica.js
+++ 1-Fundamentos/secao-04-introducao-javascript/dia-01/exercicios-agora-e-pratica.js
@@ -122,6 +122,31 @@ switch (bispo.toLocaleLowerCase()){
 //O código deve retornar uma mensagem de erro e encerrar se a nota passada for menor que 0 ou 
 //maior que 100.
 
+let nota = 1000;
+if (nota > 100){
+    console.log("Erro");
+}
+else if (nota >= 90) {
+    console.log("Você tirou A");
+}
+else if (nota >= 80) {
+    console.log("Você tirou B");
+}
+else if (nota >= 70) {
+    console.log("Você tirou C");
+}
+else if (nota >= 60){
+    console.log("Você tirou D");
+}
+else if (nota >= 50){
+    console.log("Você tirou E");
+}
+else if (nota < 50 && nota >= 0) {
+    console.log("Você tirou F");
+}
+else {
+    console.log("Erro");
+}
 
 //exercicio bonus
 const y = 15;
