6f3d47ef1e3594fc5d41114dc51617376d08f7a1
HenriqueFRodrigues <fernandeshenriquer92@gmail.com>
2022-11-27 23:31:09 -0300

    entendimento dos operadores lógicos, com senteças matemáticas

--- 1-Fundamentos/secao-04-introducao-javascript/dia-02/exercicio-aula-1.js
+++ 1-Fundamentos/secao-04-introducao-javascript/dia-02/exercicio-aula-1.js
@@ -13,11 +13,35 @@ if (candidata >= 80) {
     else {
     console.log("error");
 }
+
+const comida = 'pão na chapa';
+const bebida = 'cafézinho';
+if (bebida === 'cafézinho' && comida === 'pão na chapa') {
+  console.log('Muito obrigado pela refeição :)');
+} else {
+  console.log('Acho que houve um engano com meu pedido');
+}
+
+
+const conditionOne = true;
+const conditionTwo = false;
+console.log(conditionOne && conditionTwo);
     
+const cenouras = true;
+const leite = true;
+const arroz = true;
+const feijao = true;
+const listaDeCompras = cenouras && leite && arroz && feijao;
 
+console.log(true && true); // true
+console.log(true && false); // false
+console.log(false && true); // false
+console.log(false && false); // false
 
-
-
+let a = 4 ** 3 ** 2; // Como resolver >> 4 ** (3 ** 2); = 262144
+console.log(a);
+let b = 1 + 2 ** 3 * 4 / 5; // Como resolver = 1 +(((2 ** 3)* 4) / 5) = 7,4;
+console.log(b);
 
 let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
 
