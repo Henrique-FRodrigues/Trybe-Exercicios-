49dee74df29cb5b141d536d20c970e8e32a7feb8
HenriqueFRodrigues <fernandeshenriquer92@gmail.com>
2022-11-28 00:27:36 -0300

    adicionando conceitos de witch/case

--- 1-Fundamentos/secao-04-introducao-javascript/dia-02/exercicio-aula-1.js
+++ 1-Fundamentos/secao-04-introducao-javascript/dia-02/exercicio-aula-1.js
@@ -135,6 +135,93 @@ console.log(!undefined + " Observe esse caso"); // true >> Um valor falsy é um 
 
 console.log("------------------------------------------");
 
+//Operadores condicionais switch/case
+
+// Nesse exemplo o "switch/case" deverá imprimir no terminal a classificação de quais filmes podem ser assistidos
+// de acordo com a faixa etária de uma pessoa.
+
+let faixaEtaria = 'adulto';
+
+switch (faixaEtaria) {
+  case 'adolescente':
+    // Caso a variável seja "adolescente" então entraremos na lógica abaixo.
+
+    console.log('Consulte a classificação do filme');
+    // Quando a pessoa for adolescente essa será a mensagem impressa no console.
+
+    break;
+    
+    // O "break" serve para que o código não continue rodando caso seu resultado já tenha sido atingido
+  case 'adulto':
+    // Após implementar a lógica de um case, você pode abrir outro case.
+    // Agora vamos fazer a lógica do que deve ocorrer se a pessoa for adulta
+
+    console.log('A pessoa adulta é classificada para assistir qualquer filme')
+    //caso seja adulto essa mensagem deverá aparecer no terminal
+
+    break;
+    // Interrompemos o fluxo novamente caso a pessoa seja adulta
+    case 'idoso':
+      // Repetimos o mesmo processo para o case "idoso"
+      console.log('A pessoa idosa é classificada para assistir qualquer filme');
+      break
+  default:
+    // O "default" é utilizado como um retorno padrão caso o valor passado não encaixe em nenhum dos cases
+    // Ou seja, se a pessoa, for criança, recém nascida, ou pertencente à qualquer outra faixa etária, essa será a lógica implementada.
+    console.log('só pode assistir filmes livres');
+    // note que não é necessário utilizar o break após o default. Como todo o código já foi executado, não precisamos interromper o funcionamento do mesmo.
+}  
+
+console.log("------------------------------------------");
+
+mes = 'abril';
+let estacaoDoAno = '?';
+
+switch (mes) {
+    case 'janeiro':
+    case 'fevereiro':
+    case 'março':
+        estacaoDoAno = 'Verão';
+        break;
+    case 'abril':
+    case 'maio':
+    case 'junho':
+        estacaoDoAno = 'Outono';
+        break;
+    case 'julho':
+    case 'agosto':
+    case 'setembro':
+        estacaoDoAno = 'Inverno';
+        break;
+    case 'outubro':
+    case 'novembro':
+    case 'dezembro':
+        estacaoDoAno = 'Primavera';
+}
+//não possui um default assim o codigo não altera o primeiro valor dado pra variavel estacaoDoAno
+console.log(estacaoDoAno); // 'Outono'
+
+console.log("------------------------------------------");
+
+let statusPessoaCandidata = "reprovada";
+switch (statusPessoaCandidata){
+    case "aprovada":
+        console.log("Parabéns, você foi aprovada(o)!");
+        break;
+    case "lista":
+        console.log("Você está na nossa lista de espera");
+        break;
+    case "reprovada":
+        console.log("Você foi reprovado(a)");
+        break;
+    default:
+        console.log("Informação incorreta");
+        
+}
+
+
+console.log("------------------------------------------");
+
 let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
 
 //tasksList.push("Cuidar do bebê"); //acrescenta mais um valor para o array no final dos valores
