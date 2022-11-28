f2ab3d1faa62bd4d513ba5548f049ebca8300659
HenriqueFRodrigues <fernandeshenriquer92@gmail.com>
2022-11-28 00:31:50 -0300

    Arrumando os arquivos 1-Fundamentos/secao-04-introducao-javascript/dia-02/exercicio-aula-1.js para 1-Fundamentos/secao-04-introducao-javascript/dia-01/aula-e-seus-exercicios.js

--- 1-Fundamentos/secao-04-introducao-javascript/dia-01/exercicios-agora-e-pratica.js
+++ 1-Fundamentos/secao-04-introducao-javascript/dia-01/exercicios-agora-e-pratica.js
@@ -88,3 +88,16 @@ if (A + B + C === 180  && A > 0 && B > 0 && C > 0) {
 else {
     console.log(false);
 }
+
+//Utilize switch/case para escrever um código que receba o nome de uma peça de xadrez e retorne 
+//os movimentos que ela faz.
+//Como desafio, escreva um código para funcionar tanto se receber o nome de uma peça com letras 
+//maiúsculas quanto com letras minúsculas, sem aumentar a quantidade de condicionais.
+//Como dica, você pode pesquisar uma função que faz uma string ficar com todas as letras 
+//minúsculas (lower case).
+//Se a peça passada for inválida, o código deve retornar uma mensagem de erro.
+//Exemplo: bishop (bispo) -> diagonals (diagonais)
+//***const sentence = 'DoG';
+//console.log(sentence.toLowerCase());
+// expected output: "dog"**
+
