67910a9d6f1de3f214397cb629c7dd91e06bbbc3
HenriqueFRodrigues <fernandeshenriquer92@gmail.com>
2022-11-29 16:28:17 -0300

    deu certo os problemas do fixando aula

--- 1-Fundamentos/secao-04-introducao-javascript/dia-03/exercicio-aula-3.js
+++ 1-Fundamentos/secao-04-introducao-javascript/dia-03/exercicio-aula-3.js
@@ -12,3 +12,50 @@ if (sum > 15){
 } else {
     console.log("Valor menor que 16");
 }
+
+console.log("---------------------------");
+
+//fixando aprendizagem
+
+//Utilizando a lógica apresentada acima, qual deve ser o valor salvo na variável n
+//para que o resultado impresso pelo console.log seja 55?
+
+const n = 9 ;
+
+let resultado = 100;
+for (let index = 0; index <= n; index += 1) {
+  resultado -= index;
+};
+console.log(resultado);
+
+console.log("---------------------------");
+
+//Escolha a opção que melhor substitui a linha com o "?"
+
+const num = 5;
+
+let resultadoo = 0;
+for (let index = 0; index <= num; index += 1) {
+  resultadoo += index;
+};
+console.log(resultado)
+
+console.log("---------------------------");
+
+a = 5;
+b = 7;
+c = 3;
+d = 9;
+
+let verificarVAlor = (c == 4 || d <=6) && (a >= 5 && b !=9) || ( ! (a < 5) );
+console.log(verificarVAlor);
+
+console.log("---------------------------");
+
+let somaDeTudo = 100;
+let somandoTudo = 0;
+
+for(let indexSoma = 0; indexSoma <= somaDeTudo; indexSoma +=1){
+    somandoTudo += indexSoma;
+};
+console.log(somandoTudo);
