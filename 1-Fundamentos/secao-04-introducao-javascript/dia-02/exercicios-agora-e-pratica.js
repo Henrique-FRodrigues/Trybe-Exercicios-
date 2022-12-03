1e44e5919b8ccbbc87328ecb57cedf6e6b15395d
HenriqueFRodrigues <fernandeshenriquer92@gmail.com>
2022-12-03 00:30:37 -0300

    começando o exercicio 5

--- 1-Fundamentos/secao-04-introducao-javascript/dia-02/exercicios-agora-e-pratica.js
+++ 1-Fundamentos/secao-04-introducao-javascript/dia-02/exercicios-agora-e-pratica.js
@@ -1,23 +1,37 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-//Percorra o array imprimindo todos os valores nele contidos com a função console.log(); 
+//exercicio 1. Percorra o array imprimindo todos os valores nele contidos com a função console.log(); 
 for (let percorrer of numbers){
     console.log(percorrer);
 }
 console.log("------------------------------------------------");
 
-//Some todos os valores contidos no array e imprima o resultado; 
+//exercicio 2. Some todos os valores contidos no array e imprima o resultado; 
 let resultadaAdicao = 0;
 for (let index = 0; index < numbers.length; index += 1){  
     resultadaAdicao += numbers[index];    
 }
 console.log(resultadaAdicao);
 console.log("------------------------------------------------");
-//Calcule e imprima a média aritmética dos valores contidos no array;
-
+//exercicio 3. Calcule e imprima a média aritmética dos valores contidos no array;
     //A média aritmética é o resultado da soma de todos os elementos dividido pelo número total de elementos.
-//let divida = ;   
+
 let divisao = resultadaAdicao / numbers.length ;
 console.log(divisao);
 
-    //let divisao = resultadaAdicao / 
+console.log("------------------------------------------------");
+//exercicio 4.Com base no código que acabou de gerar, faça com que, caso o valor final seja maior que 20,
+// imprima a mensagem: “valor maior que 20”. Caso não seja, imprima a mensagem: “valor menor ou igual a 
+//20”; 
+
+if (divisao > 20){
+    console.log('valor maior que 20');
+}
+else{
+    console.log('valor menor ou igual a 20');
+}
+
+console.log("------------------------------------------------");
+//exercicio 5.Utilizando for, descubra qual o maior valor contido no array e imprima-o;
+
+console.log(numbers.sort());
