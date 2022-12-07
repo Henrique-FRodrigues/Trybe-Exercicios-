69ef4eb63b4296da4c65abab5c55c0c877fdf8d6
HenriqueFRodrigues <fernandeshenriquer92@gmail.com>
2022-12-07 15:54:35 -0300

    aprendendo sobre .getElementById

---
+++ 1-Fundamentos/secao-05-javascript-DOOM-e-seletores/dia-01-javascript-doom-seletores/javascript-da-aula-teste.js
@@ -0,0 +1,7 @@
+console.log(document.getElementById('page-title').innerText);
+
+document.getElementById('page-title').innerText = "Star Wars.";
+
+document.getElementById("paragraph").innerText = "Oi sou o Henrique e amo Star Wars, Senhor dos Anéis e tudo do mundo mágico dos filmes.";
+
+document.getElementById("subtitle").innerText = "Aprendendo sobre filmes - usando a Linguagem DOOM.";
