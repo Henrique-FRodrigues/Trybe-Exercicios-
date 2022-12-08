75b8b9b11f1e969f39393c68caf595d54c9829b6
HenriqueFRodrigues <fernandeshenriquer92@gmail.com>
2022-12-07 23:28:19 -0300

    mandando commit do exercicio de funções

--- 1-Fundamentos/secao-04-introducao-javascript/dia-04/exercicio-funcoes.js
+++ 1-Fundamentos/secao-04-introducao-javascript/dia-04/exercicio-funcoes.js
@@ -10,15 +10,13 @@
 //Faça um programa que retorne o maior de dois números.Defina, no começo do programa, duas 
 //constantes com os valores que serão comparados.
 //
-//Faça um programa que retorne o maior de três números. Defina, no começo do programa, três constantes com os valores que serão comparados.
+//Faça um programa que retorne o maior de três números. Defina, no começo do programa, três constantes 
+//com os valores que serão comparados.
 //
-//Faça um programa que, dado um valor recebido como parâmetro, retorne “positive” se esse valor for positivo, “negative” se for negativo, e caso não seja nem positivo e nem negativo retorne “zero”.
+//Faça um programa que, dado um valor recebido como parâmetro, retorne “positive” se esse valor for 
+//positivo, “negative” se for negativo, e caso não seja nem positivo e nem negativo retorne “zero”.
 //
-//Faça um programa que receba três constantes com os valores dos três ângulos internos de um triângulo. Retorne true se os ângulos representarem os ângulos de um triângulo e false, caso contrário. Se algum ângulo for inválido, o programa deve retornar uma mensagem de erro.
-//
-//    Para os ângulos serem de um triângulo válido, a soma dos três devem ser 180 graus.
-//
-//    Um ângulo será considerado inválido se não tiver um valor positivo.
+
 let a = 10;
 let b = 5;
 function adicao(a,b){
@@ -74,5 +72,58 @@ console.log("---------------");
         return d
     }
  }
-
  console.log(maior(c,d));
+
+ console.log("---------------");
+
+function qualMaior(a,b,c){
+    let arrayNovo = [];
+    let comparacaoMaior = 0;
+    arrayNovo.push(a,b,c);
+    //console.log(arrayNovo);
+    for(let index = 1; index < arrayNovo.length; index += 1){
+            if(arrayNovo[index] > comparacaoMaior){
+                comparacaoMaior = arrayNovo[index];
+            }
+        }
+    return comparacaoMaior
+}
+console.log(qualMaior(a,b,c,));
+
+console.log("---------------");
+
+function verification(a){
+    if(a > 0){
+        return 'positive'
+    }
+    else if(a < 0){
+        return 'negative'
+    }
+    else {
+        return 'zero'
+    }
+}
+console.log(verification(a));
+
+console.log("---------------");
+//Faça um programa que receba três constantes com os valores dos três ângulos internos de um triângulo. 
+//Retorne true se os ângulos representarem os ângulos de um triângulo e false, caso contrário. 
+//Se algum ângulo for inválido, o programa deve retornar uma mensagem de erro.
+//
+//    Para os ângulos serem de um triângulo válido, a soma dos três devem ser 180 graus.
+//
+//    Um ângulo será considerado inválido se não tiver um valor positivo.
+
+const angulo1 = 50;
+const angulo2 = 80;
+const angulo3 = 55;
+function triangle(angulo1, angulo2, angulo3){
+    let comparation = 180;
+    if(comparation === angulo1 + angulo2 + angulo3){
+        return true
+    }
+    else { 
+        return false
+    }
+}
+console.log(triangle(angulo1, angulo2, angulo3));
